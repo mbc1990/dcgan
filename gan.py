f705a2f385f6a570af615f81545d0e65a3e0cec1
mbc <malcolmbalch@gmail.com>
2018-01-14 16:55:45 -0500

    Working 28, 28, 3 implementation

---
+++ gan.py
@@ -0,0 +1,227 @@
+from __future__ import print_function, division
+import os
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
+from scipy.misc import imread
+
+import sys
+
+import numpy as np
+
+class DCGAN():
+    def __init__(self):
+        self.img_rows = 28 
+        self.img_cols = 28
+        self.channels = 3
+        
+        # Goldens 150x150 filtered
+        # self.img_rows = 150 
+        # self.img_cols = 150 
+        # self.channels = 1
+
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy', 
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build and compile the generator
+        self.generator = self.build_generator()
+        self.generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+        # The generator takes noise as input and generated imgs
+        z = Input(shape=(100,))
+
+        # TODO (M): img here is (28, 28, 1), when it should be (28, 28, 3)
+        img = self.generator(z)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The valid takes generated images as input and determines validity
+        valid = self.discriminator(img)
+
+        # The combined model  (stacked generator and discriminator) takes
+        # noise as input => generates images => determines validity 
+        self.combined = Model(z, valid)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+    def build_generator(self):
+
+        noise_shape = (100,)
+        
+        model = Sequential()
+        
+        # TODO (M): I'm guessing these 7 * 7 things have somethign to do with it
+        # TODO (M): because 7 is a factor of 28
+        # TODO (M): Update: Yes this is true, see model.summary() output for more info
+        model.add(Dense(128 * 7 * 7, activation="relu", input_shape=noise_shape))
+        model.add(Reshape((7, 7, 128)))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(UpSampling2D())
+        model.add(Conv2D(128, kernel_size=3, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8)) 
+        model.add(UpSampling2D())
+        model.add(Conv2D(64, kernel_size=3, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        # TODO (M): Note that changing this layer from 1 filters to 3 filters
+        # TODO (M): results in (None, 28, 28, 3) output shape
+        # model.add(Conv2D(1, kernel_size=3, padding="same"))
+        model.add(Conv2D(3, kernel_size=3, padding="same"))
+        model.add(Activation("tanh"))
+
+        # TODO (M): model.output_shape is (None, 28, 28, 1) by default
+        # TODO (M): So that means there's something in here that needs to be changed
+
+        model.summary()
+        
+        noise = Input(shape=noise_shape)
+        img = model(noise)
+
+        return Model(noise, img)
+
+    def build_discriminator(self):
+
+        img_shape = (self.img_rows, self.img_cols, self.channels)
+        
+        model = Sequential()
+
+        model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=img_shape, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+        model.add(ZeroPadding2D(padding=((0,1),(0,1))))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(1, activation='sigmoid'))
+
+        model.summary()
+
+        img = Input(shape=img_shape)
+        validity = model(img)
+
+        return Model(img, validity)
+
+    def load_input(self, img_dir):
+        """
+        Loads and formats input data
+
+        Args:
+            img_dir (string): Path to input image directory
+
+        Returns
+            Same format as mnist.load_data()
+        """
+        files = os.listdir(img_dir)
+        temp = []
+        for img_name in files:
+            image_path = os.path.join(img_dir, img_name)
+            # img = imread(image_path, flatten=True) # TODO (M): This turns (28, 28, 3) into (28, 28, 1)
+            img = imread(image_path, flatten=False)
+            img = img.astype('float32')
+            temp.append(img)
+        
+        # (M): For some reason, one of the images just ends up (28, 28)
+        # (M): so let's throw it out...
+        temp = [t for t in temp if t.shape == (28, 28, 3)]
+
+        train_x = np.stack(temp)
+        return (train_x, None), (None, None)
+
+    def train(self, epochs, batch_size=128, save_interval=50):
+
+        # Load the dataset
+        # (X_train, _), (_, _) = mnist.load_data()
+
+        (X_train, _), (_, _) = self.load_input("images_28/")
+
+        # Rescale -1 to 1
+        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+
+        # (M): This was apparently adding a 3rd dimension to input data?
+        # X_train = np.expand_dims(X_train, axis=3)
+
+        half_batch = int(batch_size / 2)
+
+        for epoch in range(epochs):
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            # Select a random half batch of images
+            idx = np.random.randint(0, X_train.shape[0], half_batch)
+            imgs = X_train[idx]
+
+            # Sample noise and generate a half batch of new images
+            noise = np.random.normal(0, 1, (half_batch, 100))
+            gen_imgs = self.generator.predict(noise)
+
+            # Train the discriminator (real classified as ones and generated as zeros)
+            d_loss_real = self.discriminator.train_on_batch(imgs, np.ones((half_batch, 1)))
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, np.zeros((half_batch, 1)))
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+
+            noise = np.random.normal(0, 1, (batch_size, 100))
+
+            # Train the generator (wants discriminator to mistake images as real)
+            g_loss = self.combined.train_on_batch(noise, np.ones((batch_size, 1)))
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+
+            # If at save interval => save generated image samples
+            if epoch % save_interval == 0:
+                self.save_imgs(epoch)
+
+    def save_imgs(self, epoch):
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, 100))
+        gen_imgs = self.generator.predict(noise)
+
+        # Rescale images 0 - 1
+        # TODO (M): Do we need to this?
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                # axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                # (M): Original author set cmap=gray which seems wrong for this use
+                axs[i,j].imshow(gen_imgs[cnt, :,:,0])
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("output/goldens_%d.png" % epoch)
+        plt.close()
+
+
+if __name__ == '__main__':
+    dcgan = DCGAN()
+    dcgan.train(epochs=4000, batch_size=32, save_interval=50)
